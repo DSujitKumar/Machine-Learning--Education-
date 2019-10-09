f4029f88fc029c9929c65d3537c91887411438a2
Sujit Kumar Das <dsujit2208@gmail.com>
2019-10-09 23:33:37 +0530

    Create SLR_1
    Just A Note

---
+++ Simple Linear Regression/SLR_1.py
@@ -0,0 +1,10 @@
+"""
+Some Simple points to remember:-
+    1. Regression provides contineous outputs.
+    2. The distance between actual points and the hyperplane is called Residue.
+    3. Error = Sum Squired Error
+        Mathematically =Σ(predicted - actual)^2 = Σ(residue)^2
+    4. Minimun residue Value is the Best model.
+    5. Mathematicaly 'y = mx+c'.
+"""
+
